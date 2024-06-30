6e8f7e8d564c755f7bf9adbdfaa1b062ed03b39f
Anjali Gupta <113882616+Anjaligupta845793@users.noreply.github.com>
2024-06-30 14:03:28 +0530

    Update Login.jsx
    meaning

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,7 +6,7 @@ const Login = (props) => {
 
        <div className='container m-auto lg:w-3/4  sm:w-full h-1/3 text-center  '>
         <p className='  mx-auto '>please use sepolia test network</p>
-        <p className='lg:text-5xl text-4xl w-3/4 mx-auto font-bold'>Welcome to Decentrlized Voting Application</p>
+        <p className='lg:text-5xl text-4xl w-3/4 mx-auto font-bold'>Welcome to Decentralized  Voting Application</p>
          <button onClick={ props.ConnectWallet} className= 'bg-white w-40 h-11 lg:mt-5 mt-10 rounded-3xl text-black font-bold hover:bg-[#cdb6fc]'>Connect Wallet</button>
        </div>
       
