66062207da0183c0ada3e2694ff13be0a7f377c5
CJCODESgit <ufotjohnamos@gmail.com>
2024-07-08 00:23:00 +0100

    first commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -6,7 +6,8 @@ const Login = (props) => {
 
        <div className='container m-auto lg:w-3/4  sm:w-full h-1/3 text-center  '>
         <p className='  mx-auto '>please use sepolia test network</p>
-        <p className='lg:text-5xl text-4xl w-3/4 mx-auto font-bold'>Welcome to Decentralized  Voting Application</p>
+        <p className='lg:text-5xl text-4xl w-3/4 mx-auto font-bold'>Welcome to Bells Blockchain voting platform</p>
+        <p className='xs:text-5xl m-12 text-4xl w-3/4 mx-auto font-bold'>Vote for your preferred candidate</p>
          <button onClick={ props.ConnectWallet} className= 'bg-white w-40 h-11 lg:mt-5 mt-10 rounded-3xl text-black font-bold hover:bg-[#cdb6fc]'>Connect Wallet</button>
        </div>
       
